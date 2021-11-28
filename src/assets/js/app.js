a40775e20e14c644d3a73729b6176c62651024d3
andrei_labadaev <andrei-labadaev@mail.ru>
2021-11-28 18:55:32 +0300

    uppdate 18.55

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,11 @@
+//= components/jquery.magnific-popup.js
+
 $('.adventure-menu__burger').click(function () {
     $('.adventure-menu__burger').toggleClass('adventure-burger__active'),
         $('.adventure-nav').toggleClass('adventure-nav__active'),
         $('body').toggleClass('adventure-look');
+});
+
+$('.adventure-news__box-foto').magnificPopup({
+    type: 'image'
 });
